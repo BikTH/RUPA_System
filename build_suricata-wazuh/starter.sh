ac06a0a6f8cc678f74744a42b2ce760604dd9353
BIKOKO Thierry <thierrybikoko8@gmail.com>
2024-11-06 02:47:01 +0100

    build ok

--- build_suricata-wazuh/starter.sh
+++ build_suricata-wazuh/starter.sh
@@ -2,6 +2,7 @@
 
 # Remplacer la variable ${WAZUH_MANAGER_IP} dans ossec.conf par la valeur de l'environnement
 sed -i "s/\${WAZUH_MANAGER_IP}/${WAZUH_MANAGER_IP}/g" /var/ossec/etc/ossec.conf
+# sed -i "s|\${WAZUH_MANAGER_IP}|${WAZUH_MANAGER_IP}|g" /var/ossec/etc/ossec.conf
 
 # Démarrer l'agent Wazuh et Suricata
 # service wazuh-agent start
@@ -11,4 +12,5 @@ sed -i "s/\${WAZUH_MANAGER_IP}/${WAZUH_MANAGER_IP}/g" /var/ossec/etc/ossec.conf
 /var/ossec/bin/wazuh-control start
 
 # Démarrer Suricata
-exec /usr/bin/suricata -c /etc/suricata/suricata.yaml -s /etc/suricata/rules/signatures.rules -i ${INT_TO_LISTEN}
+# exec /usr/bin/suricata -c /etc/suricata/suricata.yaml -s /etc/suricata/rules/signatures.rules -i ${INT_TO_LISTEN}
+exec /usr/bin/suricata -c /etc/suricata/suricata.yaml -i ${INT_TO_LISTEN}
