922faf175ae5421ae774389edb10ef692b870d9c
BIKOKO Thierry <thierrybikoko8@gmail.com>
2024-11-06 02:05:26 +0100

    maj sur le build suricata wazuh

--- build_suricata-wazuh/starter.sh
+++ build_suricata-wazuh/starter.sh
@@ -4,5 +4,11 @@
 sed -i "s/\${WAZUH_MANAGER_IP}/${WAZUH_MANAGER_IP}/g" /var/ossec/etc/ossec.conf
 
 # Démarrer l'agent Wazuh et Suricata
-service wazuh-agent start
-exec suricata -c /etc/suricata/suricata.yaml
+# service wazuh-agent start
+# exec suricata -c /etc/suricata/suricata.yaml
+
+# Démarrer l'agent Wazuh
+/var/ossec/bin/wazuh-control start
+
+# Démarrer Suricata
+exec /usr/bin/suricata -c /etc/suricata/suricata.yaml -s /etc/suricata/rules/signatures.rules -i ${INT_TO_LISTEN}
